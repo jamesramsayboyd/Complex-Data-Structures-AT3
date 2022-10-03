96a4f4893ccabfe3a8f9242bb77951dcda960dfe
James Boyd <jamesramsayboyd@gmail.com>
2022-10-03 11:31:41 +0800

    Created form with some controls

---
+++ MasterFileProject.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Complex_Data_Structures_AT3
+{
+    partial class MasterFileProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxDisplay = new System.Windows.Forms.ListBox();
+            this.listBoxFilter = new System.Windows.Forms.ListBox();
+            this.textBoxFilterId = new System.Windows.Forms.TextBox();
+            this.textBoxFilterName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxDisplay
+            // 
+            this.listBoxDisplay.FormattingEnabled = true;
+            this.listBoxDisplay.Location = new System.Drawing.Point(42, 113);
+            this.listBoxDisplay.Name = "listBoxDisplay";
+            this.listBoxDisplay.Size = new System.Drawing.Size(120, 303);
+            this.listBoxDisplay.TabIndex = 0;
+            // 
+            // listBoxFilter
+            // 
+            this.listBoxFilter.FormattingEnabled = true;
+            this.listBoxFilter.Location = new System.Drawing.Point(245, 113);
+            this.listBoxFilter.Name = "listBoxFilter";
+            this.listBoxFilter.Size = new System.Drawing.Size(120, 303);
+            this.listBoxFilter.TabIndex = 1;
+            // 
+            // textBoxFilterId
+            // 
+            this.textBoxFilterId.Location = new System.Drawing.Point(404, 132);
+            this.textBoxFilterId.Name = "textBoxFilterId";
+            this.textBoxFilterId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilterId.TabIndex = 2;
+            this.textBoxFilterId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxFilterId_KeyPress);
+            this.textBoxFilterId.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBoxFilterId_MouseDoubleClick);
+            // 
+            // textBoxFilterName
+            // 
+            this.textBoxFilterName.Location = new System.Drawing.Point(404, 190);
+            this.textBoxFilterName.Name = "textBoxFilterName";
+            this.textBoxFilterName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilterName.TabIndex = 3;
+            this.textBoxFilterName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxFilterName_KeyPress);
+            this.textBoxFilterName.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBoxFilterName_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Staff ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Staff Name:";
+            // 
+            // MasterFileProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFilterName);
+            this.Controls.Add(this.textBoxFilterId);
+            this.Controls.Add(this.listBoxFilter);
+            this.Controls.Add(this.listBoxDisplay);
+            this.Name = "MasterFileProject";
+            this.Text = "Master File Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxDisplay;
+        private System.Windows.Forms.ListBox listBoxFilter;
+        private System.Windows.Forms.TextBox textBoxFilterId;
+        private System.Windows.Forms.TextBox textBoxFilterName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MasterFileProject.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Complex_Data_Structures_AT3
+{
+    public partial class MasterFileProject : Form
+    {
+        public MasterFileProject()
+        {
+            InitializeComponent();
+            LoadStaffDetails();
+            DisplayFullData();
+        }
+
+        /// <summary>
+        /// Q4.1 Create a Dictionary data structure with a TKey of type integer
+        /// and a TValue of type string, name the new structure "MasterFile"
+        /// </summary>
+        Dictionary<int, string> MasterFile = new Dictionary<int, string>();
+        string FileName = "MyNames_v2.csv";
+
+        /// <summary>
+        /// Q4.2 Create a method that will read the data from the .csv file into
+        /// the Dictionary data structure when the form loads
+        /// </summary>
+        private void LoadStaffDetails()
+        {
+            using (StreamReader sr = new StreamReader(@FileName))
+            {
+                sr.ReadLine(); // skips first line of .csv ("mobile,name")
+                while (!sr.EndOfStream)
+                {
+                    string[] staffMember = sr.ReadLine().Split(',');
+                    int id = int.Parse(staffMember[0]);
+                    string name = staffMember[1];
+                    MasterFile.Add(id, name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Q4.3 Create a method to display the Dictionary data into a non-selectable
+        /// listbox (i.e. read only)
+        /// </summary>
+        private void DisplayFullData()
+        {
+            foreach (var staff in MasterFile)
+            {
+                listBoxDisplay.Items.Add(staff.Key);
+            }
+        }
+
+        /// <summary>
+        /// Q4.4 Create a method to filter the staff name data from the Dictionary into
+        /// a second selectable listbox. This method must use a textbox input and update
+        /// as each character is entered. The listbox must reflect the filtered data in
+        /// real time
+        /// </summary>
+        private void textBoxFilterName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            DisplayFilteredNames(textBoxFilterName.Text);
+        }
+        private void DisplayFilteredNames(string FilterName)
+        {
+            listBoxFilter.Items.Clear();
+            foreach (var staff in MasterFile)
+            {
+                if (staff.Value.ToUpper().Contains(FilterName.ToUpper()))
+                {
+                    listBoxFilter.Items.Add(staff.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Q4.5 Create a method to filter the staff ID data from the Dictionary into
+        /// a second selectable listbox. This method must use a textbox input and update
+        /// as each number is entered. The listbox must reflect the filtered data in
+        /// real time
+        /// </summary>
+        private void textBoxFilterId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            DisplayFilteredIDs(textBoxFilterId.Text);
+        }
+        private void DisplayFilteredIDs(string FilterId)
+        {
+            listBoxFilter.Items.Clear();
+            foreach (var staff in MasterFile)
+            {
+                string Id = staff.Key.ToString();
+                if (Id.Contains(FilterId))
+                {
+                    listBoxFilter.Items.Add(staff.Key);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Q4.6 Create a double click method for the staff name text box which will
+        /// clear the contents and place the focus into the staff name text box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxFilterName_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBoxFilterName.Clear();
+        }
+
+        /// <summary>
+        /// Q4.7 Create a double click method for the staff ID text box which will
+        /// clear the contents and place the focus into the staff ID text box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxFilterId_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBoxFilterId.Clear();
+        }
+    }
+}
